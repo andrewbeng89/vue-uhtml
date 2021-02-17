767cad58915f9ef0b0b6820de5506405e4693a80
andrewbeng89 <andrewbeng89@gmail.com>
2021-02-17 11:53:37 +0800

    clean up dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,5 @@
-// import babel from "rollup-plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
 import urlResolve from "rollup-plugin-url-resolve";
 import { terser } from "rollup-plugin-terser";
-
-const extensions = [".js"];
 
 export default {
   input: "src/index.js",
@@ -12,12 +8,6 @@ export default {
     format: "es"
   },
   plugins: [
-    resolve({ extensions }),
-    // babel({
-    //   extensions,
-    //   include: ["src/**/*"],
-    //   runtimeHelpers: true
-    // }),
     urlResolve(),
     terser()
   ]
