7ccdf4da06e385daab9ad25290640f8e6e051217
andrewbeng89 <andrewbeng89@gmail.com>
2021-05-07 14:00:59 +0800

    configure modern and legacy builds

--- rollup.config.js
+++ rollup.config.js
@@ -7,44 +7,60 @@ import copy from "rollup-plugin-copy";
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
 import fg from "fast-glob";
-import { babel } from "@rollup/plugin-babel";
+import { babel, getBabelOutputPlugin } from "@rollup/plugin-babel";
 
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
 const outputDir = process.env.NODE_ENV === "dev" ? "./dev" : "./dist";
-const outputCJS =
-  process.env.NODE_ENV === "dev" ? "dev/index.cjs.js" : "dist/index.cjs.js";
+const outputUMD =
+  process.env.NODE_ENV === "dev"
+    ? "dev/index.legacy.js"
+    : "dist/index.legacy.js";
 const outputESM =
   process.env.NODE_ENV === "dev" ? "dev/index.esm.js" : "dist/index.esm.js";
+
+const pluginWatch = () => ({
+  name: "watch-external",
+  async buildStart() {
+    const files = await fg("src/**/*");
+    files.forEach((file) => {
+      this.addWatchFile(file);
+    });
+  },
+});
 
 export default [
   {
     input: "src/index.js",
     output: [
       {
-        file: outputCJS,
-        sourcemap: true,
-        format: "cjs",
-      },
-      {
         file: outputESM,
         sourcemap: true,
         format: "esm",
+        plugins: [
+          getBabelOutputPlugin({
+            presets: [["@babel/preset-modules"]],
+          }),
+        ],
       },
     ],
     plugins: [
-      {
-        name: "watch-external",
-        async buildStart() {
-          const files = await fg("src/**/*");
-          files.forEach((file) => {
-            this.addWatchFile(file);
-          });
-        },
-      },
+      pluginWatch(),
       commonjs(),
-      babel({ babelHelpers: "bundled" }),
+      babel({
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: { esmodules: true },
+              bugfixes: true,
+              loose: true,
+            },
+          ],
+        ],
+        babelHelpers: "bundled",
+      }),
       replace({
         "process.env.NODE_ENV": JSON.stringify("production"),
       }),
@@ -65,6 +81,47 @@ export default [
       ...(process.env.NODE_ENV === "production"
         ? []
         : [serve("dev"), livereload("dev")]),
+    ],
+  },
+  {
+    input: "src/index.legacy.js",
+    output: [
+      {
+        file: outputUMD,
+        sourcemap: true,
+        name: "VueUhtml",
+        entryFileNames: "[name].legacy.js",
+        chunkFileNames: "[name]-[hash].legacy.js",
+        format: "umd",
+        plugins: [
+          getBabelOutputPlugin({
+            presets: [["@babel/preset-env"]],
+            allowAllFormats: true,
+          }),
+        ],
+      },
+    ],
+    plugins: [
+      pluginWatch(),
+      commonjs(),
+      babel({
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: { esmodules: true },
+              bugfixes: true,
+              loose: true,
+            },
+          ],
+        ],
+        babelHelpers: "bundled",
+      }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      }),
+      nodeResolve(),
+      terser(),
     ],
   },
   ...(process.env.NODE_ENV === "dev"
